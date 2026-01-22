53381db19c6400527c45d458c62b045e3bad5860
Igor Solovev <solov.ig0r.1111@yandex.ru>
2026-01-22 15:04:52 +0300

    init services

---
+++ api-gateway/src/main/java/com/morzevichka/api_gateway/TestFilter.java
@@ -0,0 +1,9 @@
+package com.morzevichka.api_gateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.HandlerFilterFunction;
+
+@Configuration
+public class TestFilter {
+
+}
